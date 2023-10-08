3804058639c235d1afc470d14810a421eca87406
Egor Gaychukov <egor.gaychukov2000.20@gmail.com>
2023-10-08 14:34:21 +0300

    Uploaded the template of the application

---
+++ QuizApp/QuizApp/Controllers/QuestionController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuizApp.Models;
+
+namespace QuizApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class QuestionController : ControllerBase
+    {
+        [HttpGet("{category}/{number:int}")]
+        public string Get([FromRoute]QuestionRequest request)
+        {
+            return $"{request.Category} {request.Number}";
+        }
+    }
+}
---
+++ QuizApp/QuizApp/Models/QuestionRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace QuizApp.Models
+{
+    public class QuestionRequest
+    {
+        public int Number { get; set; }
+        public string Category { get; set; }
+    }
+}
