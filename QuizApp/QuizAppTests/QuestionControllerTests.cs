99863178f39a033a3f8707b37a65a407fa4a2bbe
Egor Gaychukov <egor.gaychukov2000.20@gmail.com>
2023-10-09 21:04:54 +0300

    Mocked HttpMessageHandler

--- QuizApp/QuizAppTests/QuestionControllerTests.cs
+++ QuizApp/QuizAppTests/QuestionControllerTests.cs
@@ -1,10 +1,33 @@
+using Moq;
+using Moq.Protected;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+
 namespace QuizAppTests
 {
     public class QuestionControllerTests
     {
         public QuestionControllerTests()
         {
-            
+
+        }
+
+        private static Mock<HttpMessageHandler> GetMessageHandlerMock(HttpStatusCode statusCode, string responseContent)
+        {
+            var mockHttpMessageHandler = new Mock<HttpMessageHandler>();
+
+            mockHttpMessageHandler.Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                    "SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage>(),
+                    ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(new HttpResponseMessage
+                {
+                    StatusCode = statusCode,
+                    Content = new StringContent(responseContent, Encoding.UTF8, "application/json")
+                });
+            return mockHttpMessageHandler;
         }
     }
 }
