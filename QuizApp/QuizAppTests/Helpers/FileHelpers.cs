5ab39fc552ff236993b6676eff06f79652a1e165
Egor Gaychukov <egor.gaychukov2000.20@gmail.com>
2023-10-10 00:25:11 +0300

    Added some test data and methods to manage the data

---
+++ QuizApp/QuizAppTests/Helpers/FileHelpers.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+using System.Text.Json;
+
+namespace QuizAppTests.Helpers
+{
+    internal static class FileHelpers
+    {
+        private const string ResponseContentDirectory = "TestResponseContent";
+
+        public static T GetEntityFromJson<T>(string jsonFileName)
+        {
+            var jsonFileContent = GetResponseContent(jsonFileName);
+            var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+            var result = JsonSerializer.Deserialize<T>(jsonFileContent, options);
+            return result;
+        }
+
+        public static string GetResponseContent(string contentFileName)
+        {
+            var contentFilePath = Path.Combine($".\\{ResponseContentDirectory}", $"{contentFileName}.json");
+            return File.ReadAllText(contentFilePath);
+        }
+    }
+}
