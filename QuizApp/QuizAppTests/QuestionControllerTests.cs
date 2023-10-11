1ff5fb75db73c6ab446854345ef0a804480483e9
Egor Gaychukov <egor.gaychukov2000.20@gmail.com>
2023-10-11 22:42:23 +0300

    Changed the name of a method to meet the convention

--- QuizApp/QuizAppTests/QuestionControllerTests.cs
+++ QuizApp/QuizAppTests/QuestionControllerTests.cs
@@ -9,7 +9,7 @@ namespace QuizAppTests
     public class QuestionControllerTests
     {
         [Fact]
-        public async Task Get_ValidRequestParameters_QuestionNumberRemainsNotAffected()
+        public async Task GetAsync_ValidRequestParameters_QuestionNumberRemainsNotAffected()
         {
             // Arrange 
             var responseTextContent = FileHelpers.GetResponseContent("HistoryQuestions");
@@ -18,14 +18,14 @@ namespace QuizAppTests
             var questionRequest = new QuestionRequest() { Number = 2, Category = Category.History };
 
             // Act
-            var questions = await questionController.Get(questionRequest);
+            var questions = await questionController.GetAsync(questionRequest);
 
             // Assert
             Assert.Equal(questionRequest.Number, questions.Count());
         }
 
         [Fact]
-        public async Task Get_ValidRequestParameters_QuestionsAreNotModified()
+        public async Task GetAsync_ValidRequestParameters_QuestionsAreNotModified()
         {
             // Arrange
             var responseTextContent = FileHelpers.GetResponseContent("SingleAnimalQuestion");
@@ -35,7 +35,7 @@ namespace QuizAppTests
             var expectedQuestion = questionResponse.Results.FirstOrDefault();
 
             // Act
-            var questions = await questionController.Get(new QuestionRequest());
+            var questions = await questionController.GetAsync(new QuestionRequest());
 
             // Assert
             var actualQuestion = questions.FirstOrDefault();
@@ -46,14 +46,14 @@ namespace QuizAppTests
         }
 
         [Fact]
-        public async Task Get_InternalApiResponsesWithError_ThrowsHttpRequestException()
+        public async Task GetAsync_InternalApiResponsesWithError_ThrowsHttpRequestException()
         {
             // Arrange
             var httpClientFactory = HttpClientHelpers.GetHttpClientFactoryMock(HttpStatusCode.NotFound, string.Empty);
             var questionController = new QuestionController(httpClientFactory.Object);
 
             // Act & Assert
-            await Assert.ThrowsAsync<HttpRequestException>(async () => await questionController.Get(new QuestionRequest()));
+            await Assert.ThrowsAsync<HttpRequestException>(async () => await questionController.GetAsync(new QuestionRequest()));
         }
     }
 }
